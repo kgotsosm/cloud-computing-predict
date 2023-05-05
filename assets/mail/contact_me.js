b03a9d35576d7e6196b33cad7994c79a11efb7e3
Kgotso Makhalimele <89485688+kgotsosm@users.noreply.github.com>
2023-05-05 18:45:18 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://sc7gqimvy7.execute-api.eu-west-1.amazonaws.com/cloud-predict}",
+                url: "{https://wgubdde453.execute-api.eu-west-1.amazonaws.com/default/cloud-predict}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
